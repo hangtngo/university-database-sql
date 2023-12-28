f18b096a86e8434ebef8cd483ba1a8313edaa4d4
Hang <65748148+hangtngo@users.noreply.github.com>
2023-12-27 23:21:37 -0700

    Update DatabaseProjectFinal.sql

--- DatabaseProjectFinal.sql
+++ DatabaseProjectFinal.sql
@@ -1,10 +1,4 @@
-/*
-INFO 4140: Business Databases
-Project: Final Project
-Group number: 4
-Group Name: Jacinta Gitahi, Jay Hanstrom, Toyin Alli, Ryan Wells, Hang Ngo
-Section Number: 1
-*/
+
 
 -- Create the database
 
